cc374f7f604777fc974f0fa55e33ea18df7b95d0
guozhen <guozhen@bytedance.com>
2019-01-01 17:21:55 +0800

    Site updated: 2019-01-01 17:21:55

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,1 @@
-[{"title":"first_article","date":"2019.01.01","uri":"2019/01/01/first-article/","excerpt":""},{"title":"Hello World","date":"2019.01.01","uri":"2019/01/01/hello-world/","excerpt":""}]
+[{"title":"2018 && 2019","date":"2019.01.01","uri":"2019/01/01/2018 && 2019/","excerpt":""}]
